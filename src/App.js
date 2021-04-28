bec14f035640374f886064054bdc0f58bff128ee
Riley Loudon <rileyloudon@icloud.com>
2021-04-27 22:32:20 -0500

    setup contact inputs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import React from 'react';
 import './App.css';
+import ContactInfo from './components/Contact';
 
-function App() {
-  return ();
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      contactText: { name: '', address: '', city: '', phone: '', email: '' },
+      contactItems: [],
+      education: [],
+      workExperience: [],
+    };
+  }
+
+  updateContactText = (e, textField) => {
+    this.setState({
+      contactText: {
+        name: textField === 'Name' ? e.target.value : this.state.contactText.name,
+        address:
+          textField === 'Address' ? e.target.value : this.state.contactText.address,
+        city: textField === 'City' ? e.target.value : this.state.contactText.city,
+        phone: textField === 'Phone' ? e.target.value : this.state.contactText.phone,
+        email: textField === 'Email' ? e.target.value : this.state.contactText.email,
+      },
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <header>Resume</header>
+        <ContactInfo
+          contactText={this.state.contactText}
+          updateInput={this.updateContactText}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,1 +1,43 @@
 import '../styles/Contact.css';
+
+const ContactInfo = (props) => {
+  const { contactText, updateInput } = props;
+
+  return (
+    <form>
+      <h1>Contact Info</h1>
+      <input
+        type='text'
+        placeholder='Name'
+        value={contactText.name}
+        onChange={(e) => updateInput(e, 'Name')}
+      />
+      <input
+        type='text'
+        placeholder='Address'
+        value={contactText.address}
+        onChange={(e) => updateInput(e, 'Address')}
+      />
+      <input
+        type='text'
+        placeholder='City'
+        value={contactText.city}
+        onChange={(e) => updateInput(e, 'City')}
+      />
+      <input
+        type='tel'
+        placeholder='Phone Number'
+        value={contactText.phone}
+        onChange={(e) => updateInput(e, 'Phone')}
+      />
+      <input
+        type='email'
+        placeholder='Email'
+        value={contactText.email}
+        onChange={(e) => updateInput(e, 'Email')}
+      />
+    </form>
+  );
+};
+
+export default ContactInfo;
