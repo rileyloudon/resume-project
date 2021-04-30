a129bc8c2706d6227a0b34763bc6a556a2143842
Riley Loudon <rileyloudon@icloud.com>
2021-04-29 19:50:17 -0500

    allow work experience to be added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import ContactInfo from './components/Contact';
+import Education from './components/Eduction';
+import WorkExperience from './components/WorkExperience';
 
 class App extends React.Component {
   constructor() {
@@ -8,9 +10,14 @@ class App extends React.Component {
 
     this.state = {
       contactText: { name: '', address: '', city: '', phone: '', email: '' },
-      contactItems: [],
-      education: [],
-      workExperience: [],
+      workItems: [],
+      workText: {
+        company: '',
+        dateStart: '',
+        dateEnd: '',
+        currentJob: false,
+        description: '',
+      },
     };
   }
 
@@ -27,6 +34,56 @@ class App extends React.Component {
     });
   };
 
+  addWork = (e) => {
+    const { workText } = this.state;
+
+    e.preventDefault();
+    if (
+      workText.company === '' ||
+      workText.dateStart === '' ||
+      (!workText.currentJob && workText.lastDay === '')
+    )
+      return;
+
+    this.setState({
+      workItems: this.state.workItems.concat(this.state.workText),
+      workText: {
+        company: '',
+        currentJob: false,
+        dateStart: '',
+        dateEnd: '',
+        description: '',
+      },
+    });
+  };
+
+  updateWorkText = (e, textField) => {
+    this.setState({
+      workText: {
+        company:
+          textField === 'Company' ? e.target.value : this.state.workText.company,
+
+        currentJob:
+          textField === 'Current Job'
+            ? e.target.checked
+            : this.state.workText.currentJob,
+
+        dateStart:
+          textField === 'Date Start'
+            ? e.target.value
+            : this.state.workText.dateStart,
+
+        dateEnd:
+          textField === 'Date End' ? e.target.value : this.state.workText.dateEnd,
+
+        description:
+          textField === 'Description'
+            ? e.target.value
+            : this.state.contactText.email,
+      },
+    });
+  };
+
   render() {
     return (
       <div>
@@ -35,6 +92,13 @@ class App extends React.Component {
           contactText={this.state.contactText}
           updateInput={this.updateContactText}
         />
+        <WorkExperience
+          workItems={this.state.workItems}
+          workText={this.state.workText}
+          addWork={this.addWork}
+          updateWorkText={this.updateWorkText}
+        />
+        <Education />
       </div>
     );
   }
--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,1 +1,103 @@
 import '../styles/WorkExperience.css';
+
+const EndDate = (props) => {
+  const { workText, updateWorkText } = props;
+  return (
+    <span>
+      <label htmlFor='dateEnd'>Last Day</label>
+      <input
+        id='dateEnd'
+        type='text'
+        value={workText.dateEnd}
+        onChange={(e) => updateWorkText(e, 'Date End')}
+      />
+    </span>
+  );
+};
+
+const WorkForm = (props) => {
+  const { workText, updateWorkText, addWork } = props;
+  const showEndDate = workText.currentJob ? null : (
+    <EndDate workText={workText} updateWorkText={updateWorkText} />
+  );
+
+  return (
+    <form>
+      <label htmlFor='company'>Company Name</label>
+      <input
+        id='company'
+        type='text'
+        placeholder='Company'
+        value={workText.company}
+        onChange={(e) => updateWorkText(e, 'Company')}
+        required
+      />
+
+      <label htmlFor='currentJob'>Still working there?</label>
+      <input
+        id='currentJob'
+        type='checkbox'
+        name='currentJob'
+        checked={workText.currentJob}
+        onChange={(e) => updateWorkText(e, 'Current Job')}
+      />
+
+      <label htmlFor='dateStart'>Date Started</label>
+      <input
+        id='dateStart'
+        type='text'
+        value={workText.dateStart}
+        onChange={(e) => updateWorkText(e, 'Date Start')}
+        required
+      />
+
+      {showEndDate}
+
+      <label htmlFor='jobDescription'>Job Description</label>
+      <textarea
+        id='jobDescription'
+        name='jobDescriptuon'
+        placeholder='Job Description'
+        value={workText.description}
+        onChange={(e) => updateWorkText(e, 'Description')}
+      />
+      <button type='submit' onClick={addWork}>
+        Add Job
+      </button>
+    </form>
+  );
+};
+
+const WorkItem = (props) => {
+  const { work } = props;
+
+  return (
+    <div>
+      <p>
+        <span>{work.company}</span> {work.dateStart} -{' '}
+        {work.currentJob ? 'Present' : work.dateEnd}
+      </p>
+      <p>{work.description}</p>
+    </div>
+  );
+};
+
+const WorkExperience = (props) => {
+  const { workText, workItems, updateWorkText, addWork } = props;
+
+  return (
+    <div>
+      <h1>Work Experience</h1>
+      <WorkForm
+        workText={workText}
+        updateWorkText={updateWorkText}
+        addWork={addWork}
+      />
+      {workItems.map((work) => (
+        <WorkItem work={work} />
+      ))}
+    </div>
+  );
+};
+
+export default WorkExperience;
